f6f103a9b395536ca70f7d8b3f93840f42a80a60
0Madhuranga0 <116251542+HansakaShehan@users.noreply.github.com>
2025-05-09 21:50:59 +0530

    change url call action

--- partials/add-task-floating-button.php
+++ partials/add-task-floating-button.php
@@ -1,4 +1,4 @@
-<a href="./add-task.php">
+<a href="<?php echo env('APP_URL').'/add-task.php' ?>">
     <button type="button" class="btn btn-primary rounded-circle position-fixed"
         style="bottom: 20px; right: 20px; width: 60px; height: 60px;">
         +
